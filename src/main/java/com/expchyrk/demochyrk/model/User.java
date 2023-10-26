ec4aec7afd49424ca4452907ffd2fc610e441bb5
Chernienko Evgeny <edcher2006@gmail.com>
2023-10-26 18:10:15 +0500

    Ver-1.1

--- src/main/java/com/expchyrk/demochyrk/model/User.java
+++ src/main/java/com/expchyrk/demochyrk/model/User.java
@@ -1,8 +1,6 @@
 package com.expchyrk.demochyrk.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -10,6 +8,7 @@ import lombok.Data;
 @Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
     private String password;
